ca87ca8d0b5f3aaffda8116bf7a705940fb8e63a
Woo Chia Wei <chiaweiwoo123@gmail.com>
2017-07-29 23:17:45 +0800

    Refactoring on configs and scraping algorithm.

--- main.py
+++ main.py
@@ -2,45 +2,47 @@ import requests
 import json
 from bs4 import BeautifulSoup
 
+# Load configurations
+with open('config.json') as config:
+    settings = json.load(config)
+    delimiter = settings["delimiter"]
+    targetUrls = settings["urls"]
+
 # web scrap individual staff url
 # data has set type, no duplicated values
 # nullEmails is indexes of nth-staff who does not have email on site
-def scrap(url, data, nullEmails = []):
+def scrap(url, data):
     # Initialization
+    names = []
+    titles = []
+    emails = []
     html = requests.get(url).text
     soup = BeautifulSoup(html, 'html.parser')
+    profiles = soup.select("#ISS_Main_T467AEC33012_Col00 .block-profile-thumb");
 
-    # Start web scraping
-    names = soup.select("#ISS_Main_T467AEC33012_Col00 h2 a")
-    names = map(lambda x: x.text.strip(), names)
-    names = list(names)
+    for profile in profiles:
+        element_name = profile.select_one("h2 a")
+        element_title = profile.select_one("h3")
+        element_email = profile.select_one("my-email")
 
-    titles = soup.select("#ISS_Main_T467AEC33012_Col00 h3")
-    titles = titles[0::2]
-    titles = map(lambda x: x.text.strip(), titles)
-    titles = list(titles)
+        # Skip the records if is blank profile
+        if (element_name is None and
+                    element_title is None and
+                    element_email is None): continue
 
-    emails = soup.select("#ISS_Main_T467AEC33012_Col00 my-email");
-    emails = map(lambda x: x["data-user"].strip()+"@"+x["data-domain"].strip(), emails)
-    emails = list(emails)
+        # At least one of the attribute is not blank
+        # Save the data in corresponding list
+        names.append(element_name.text.strip() if element_name is not None else "")
+        titles.append(element_title.text.strip() if element_title is not None else "")
+        emails.append(element_email["data-user"].strip() + "@" + element_email[
+            "data-domain"].strip() if element_email is not None else "")
 
-    # Insert blank entry into emails
-    for i in nullEmails:
-        emails.insert(i, "")
-
-    # Error checking. Make sure data count is same, else throw an exception
-    dataCounts = [len(names), len(titles), len(emails)]
-    if(len(set(dataCounts)) > 1):
-        raise Exception("The data count is different in " + url);
-
-    # Print all the data
-    count = len(names)
-    for i in range(0, count):
-        data.add('|'.join([names[i],
-                           titles[i],
-                           emails[i]]))
+    # Store the concatenated values with delimiter
+    for i in range(0, len(names)):
+        data.add(delimiter.join([names[i], titles[i], emails[i]]))
 
     print("scrap() executed successfully for " + url)
+
 
 def scrapAll(urls):
     # Define a set for storing data to avoid duplication.
@@ -48,21 +50,18 @@ def scrapAll(urls):
 
     # Special case, missing emails for some staffs
     for url in urls:
-        if(url=="https://www.iss.nus.edu.sg/about-us/iss-team/adjunct-staff"):
-            scrap(url, data, [5])
-        elif(url=="https://www.iss.nus.edu.sg/about-us/iss-team/adjunct-staff/2"):
-            scrap(url, data, [0])
-        else:
-            scrap(url, data)
+        scrap(url, data)
 
     # Write data into csv file
     with open('data.csv', 'w') as csvWriter:
+        # Print header
+        csvWriter.write(delimiter.join(["Name", "Title", "Email(s)"]) + "\n")
+
+        # Print body
         for d in data:
             csvWriter.write(d + "\n")
 
     print("scrapAll() executed successfully.")
 
-with open('config.json') as config:
-    urls = json.load(config)["urls"]
-
-scrapAll(urls)
+# Main execution
+scrapAll(targetUrls)
